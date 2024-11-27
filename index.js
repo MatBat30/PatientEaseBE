fb15ba85d47c0a75e31a6ca30c2080a06ecf8dad
ThibautHI07 <thib.h10@gmail.com>
2024-11-27 09:56:17 +0100

    Adding swagger API, signin/signpu

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const dotenv = require("dotenv")
 dotenv.config();
 const port = process.env.PORT
 
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs"); // Pour lire le fichier swagger.json
+const path = require("path"); // Pour résoudre le chemin du fichier
+
 // --- ROUTER ---
 const auth = require('./router/auth')
 
@@ -15,11 +19,20 @@ app.use(cors({
     origin: true,
     methods: ['GET', 'POST', 'PUT'],
     credentials: true
-  }));
+}));
+
+
+// Charger le fichier swagger.json
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "swagger.json"), "utf8")
+);
+
+// Route Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // --- PATH ---
 app.get('/', (req, res) => {
-    res.json({message: 'Hello world !'})
+    res.json({ message: 'Hello world !' })
 })
 
 app.use('/auth', auth)
@@ -27,4 +40,5 @@ app.use('/auth', auth)
 // --- START SERVER ---
 app.listen(port, () => {
     console.log('App listen on port : ' + port)
+    console.log(`Documentation Swagger : http://localhost:${port}/api-docs`);
 })
